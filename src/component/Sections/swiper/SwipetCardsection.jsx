a9e7babef5cdcca54c625c7e333db679445ab7c4
janmenjaya <janmenjayabiswal@gmail.com>
2024-04-15 13:59:32 +0530

    som changes

--- src/component/Sections/swiper/SwipetCardsection.jsx
+++ src/component/Sections/swiper/SwipetCardsection.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-function SwipetCardsection({ alt, img }) {
-  return (
-    <>
-      <div className="row">
-        <div className="col col-md-12 col-sm-12 col-lg-12 col-xl-12">
-          <div className="card rounded-5 swiper-card">
-            <img
-              alt={alt}
-              src={img}
-              height="100%"
-              width="100%"
-              className="rounded-5"
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default SwipetCardsection;
-=======
 import React from "react";
 
 function SwipetCardsection({ alt, img }) {
@@ -48,4 +23,3 @@ function SwipetCardsection({ alt, img }) {
 }
 
 export default SwipetCardsection;
->>>>>>> 7a7e236b96e4826b37126ea089515647f5ba6407
